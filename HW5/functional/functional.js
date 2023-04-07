960cd8e284225577ecbcdff4dcd67cd276cbdfcf
Matuma1ni <nadejdav90@gmail.com>
2023-04-07 17:47:02 +0300

    add searching and moving to list of completed tasks, add some design

--- HW5/functional/functional.js
+++ HW5/functional/functional.js
@@ -1,6 +1,7 @@
 (function () {
     let state = undefined;
     let doneState = undefined;
+    let searchString = undefined;
 
     /**
      * Global application state
@@ -18,6 +19,7 @@
 
         return [state, setValue];
     }
+
     function useDoneState(initialValue) {
         doneState = doneState || initialValue;
 
@@ -29,34 +31,88 @@
         return [doneState, setValue];
     }
 
+    function useSearchString(initialValue) {
+        searchString = searchString || initialValue;
+
+        function setValue(newValue) {
+            searchString = newValue;
+            renderApp();
+        }
+        return [searchString, setValue];
+    }
+
     /**
      * Functional component for the list
      * @param items {string[]}
      * @returns {HTMLElement} - List element
      */
-    function List({items, onDeleteTask}) {
+    function List({items, searchString, onDeleteTask, onCompleteTask}) {
         /* const listItems = items.map((item) => `<li>${item}</li>`).join(""); */
         const ul = document.createElement("ul");
         const listItems = [];
         for (let i=0; i<items.length; i++) {
-            ul.append(ListElement({item: items[i], onDeleteTask: () => onDeleteTask(i)}));
+            if (searchString) {
+                if (items[i].includes(searchString)) {
+                    ul.append(ListElement({item: items[i],
+                        onDeleteTask: () => onDeleteTask(i),
+                        onCompleteTask: () => onCompleteTask(i)
+                        }))
+                }
+            } else {
+            ul.append(ListElement({item: items[i],
+                                   onDeleteTask: () => onDeleteTask(i),
+                                   onCompleteTask: () => onCompleteTask(i)
+                                   }));
+            }
         }
         /* ul.innerHTML = listItems; */
         return ul;
     }
-    function ListElement({item, onDeleteTask}) {
+
+    function ListElement({item, onDeleteTask, onCompleteTask}) {
         let li = document.createElement("li");
-        const deleteButton = Button({text: "🗑", onClick: onDeleteTask});
-        const checkbox = document.createElement("INPUT");
-        checkbox.setAttribute("type", "checkbox");
-        li.append(checkbox, `${item}`, deleteButton);
+        const deleteImage = document.createElement("img");
+        deleteImage.setAttribute("src", "static/deleteButton.svg");
+        deleteImage.classList.add("deleteImage");
+        const deleteButton = Button({text: "", onClick: onDeleteTask});
+        deleteButton.append(deleteImage);
+        deleteButton.classList.add("deleteButton");
+
+        const checkbox = document.createElement("img");
+        checkbox.setAttribute("src", "static/checkboxTask.svg");
+        /*checkbox.onchange = function(){onCompleteTask}; */
+        checkbox.addEventListener("click", onCompleteTask);
+        let span = document.createElement("span");
+        span.innerHTML = `${item}`;
+        span.classList.add('spanTask');
+
+        li.append(checkbox, span, deleteButton);
         return li;
     }
-    function DoneList({doneItems}) {
-        const listItems = doneItems.map((item) => `<li>${item}</li>`).join("");
+
+    function DoneList({doneItems, searchString}) {
         const ul = document.createElement("ul");
-        ul.innerHTML = listItems;
+        for (let i=0; i<doneItems.length; i++) {
+            if (searchString) {
+                if (doneItems[i].includes(searchString)) {
+                    ul.append(DoneListElement({doneItem: doneItems[i]}));
+                };
+            } else {
+                ul.append(DoneListElement({doneItem: doneItems[i]}));
+            }
+        }
         return ul;
+    }
+    
+    function DoneListElement({doneItem}) {
+        let li = document.createElement("li");
+        const checkboxSVG = document.createElement("img");
+        checkboxSVG.setAttribute("src", "static/checkboxDone.svg");
+        let span = document.createElement("span");
+        span.innerHTML = `${doneItem}`;
+        span.classList.add("spanDone");
+        li.append(checkboxSVG, span);
+        return li;
     }
 
     /**
@@ -72,11 +128,12 @@
         return button;
     }
 
-    function TextInput(placeholder) {
+    function TextInput(searchString, placeholder, changeSearchString) {
         const textInput = document.createElement("INPUT");
         textInput.setAttribute("type", "text");
         textInput.placeholder = placeholder;
-        let value = textInput.value;
+        textInput.value = searchString;
+        textInput.addEventListener("change", () => changeSearchString());
         return textInput;
     }
 
@@ -88,9 +145,11 @@
     function App() {
         const [items, setItems] = useState(["Task 1 Title", "Task 2 Title", "Task 3 Title"]);
         const [doneItems, setDoneItems] = useDoneState(["Completed Task 1 Title", "Completed Task 2 Title"]);
+        const [searchString, setSearchString] = useSearchString('');
 
         function addNewTask() {
-            divPopup.style.display = "block";
+            popupOverlay.style.display = "block";
+
         }
 
         function addItem() {
@@ -99,7 +158,7 @@
         }
 
         function closePopup() {
-            divPopup.style.display = "none";
+            popupOverlay.style.display = "none";
         }
 
         function deleteTask(i) {
@@ -107,24 +166,57 @@
             setItems(items);
         }
 
+        function completeTask(i) {
+            let element = items.slice(i, 1);
+            items.splice(i, 1);
+            setDoneItems([...doneItems, element]);
+            setItems(items);
+        }
+
+        function changeSearchString() {
+            setSearchString(textInput.value);
+        }
+
         const div = document.createElement("div");
-        const list = List({items, onDeleteTask: deleteTask});
-        const doneList = DoneList({doneItems});
+        div.classList.add("mainDiv");
+        const listsDiv = document.createElement("div");
+        listsDiv.classList.add("listsDiv");
+        const list = List({items, onDeleteTask: deleteTask, onCompleteTask: completeTask, searchString});
+        const doneList = DoneList({doneItems, searchString});
+
         const newTaskButton = Button({text: "+ New Task", onClick: addNewTask});
-        const textInput = TextInput("Search Task");
+        newTaskButton.classList.add("buttonNewTask");
+        const textInput = TextInput(searchString, "Search Task", changeSearchString);
+        textInput.classList.add("textInput");
         const header = document.createElement("h1");
         header.innerHTML = "To Do List";
+
         const taskHeader = document.createElement("h3");
         taskHeader.innerHTML = "All Tasks";
         const doneHeader = document.createElement("h3");
         doneHeader.innerHTML = "Completed Tasks";
+        listsDiv.append(taskHeader, list, doneHeader, doneList,)
+        
+        const popupOverlay = document.createElement("div");
+        popupOverlay.classList.add("popupOverlay");
         const divPopup = document.createElement("div");
+        divPopup.classList.add("divPopup");
+        const popupHeader = document.createElement("h3");
+        popupHeader.innerHTML = "Add New Task";
+        const divPopupButtons = document.createElement("div");
+        divPopupButtons.classList.add("divPopupButtons");
         const popupTextInput = TextInput("New Task");
+        popupTextInput.classList.add("popupTextInput");
         const addTaskButton = Button({text: "Add", onClick: addItem});
+        addTaskButton.classList.add("popupSubmitButton");
         const closePopupButton = Button({text: "Cancel", onClick: closePopup});
-        divPopup.append(popupTextInput, closePopupButton, addTaskButton);
-        divPopup.style.display = "none";
-        div.append(header, textInput, newTaskButton, taskHeader, list, doneHeader, doneList, divPopup);
+        closePopupButton.classList.add("popupCancelButton");
+        popupOverlay.append(divPopup);
+        divPopupButtons.append(closePopupButton, addTaskButton);
+        divPopup.append(popupHeader, popupTextInput, divPopupButtons);
+        popupOverlay.style.display = "none";
+
+        div.append(header,  popupOverlay, textInput, newTaskButton, listsDiv,);
         return div;
     }
 
