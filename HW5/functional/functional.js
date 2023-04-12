6b6f11c70ad4785ffd1ff696616809a46462c044
Matuma1ni <nadejdav90@gmail.com>
2023-04-12 10:32:25 +0300

    refactoring

--- HW5/functional/functional.js
+++ HW5/functional/functional.js
@@ -47,7 +47,6 @@
      * @returns {HTMLElement} - List element
      */
     function List({items, searchString, onDeleteTask, onCompleteTask}) {
-        /* const listItems = items.map((item) => `<li>${item}</li>`).join(""); */
         const ul = document.createElement("ul");
         const listItems = [];
         for (let i=0; i<items.length; i++) {
@@ -65,7 +64,6 @@
                                    }));
             }
         }
-        /* ul.innerHTML = listItems; */
         return ul;
     }
 
@@ -73,12 +71,6 @@
         let li = document.createElement("li");
         const deleteImage = document.createElement("span");
         deleteImage.innerHTML = '<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 2H8C8 1.44772 7.55228 1 7 1C6.44772 1 6 1.44772 6 2ZM5 2C5 0.89543 5.89543 0 7 0C8.10457 0 9 0.89543 9 2H13C13.2761 2 13.5 2.22386 13.5 2.5C13.5 2.77614 13.2761 3 13 3H12.4364L11.2313 11.8378C11.0624 13.0765 10.0044 14 8.75422 14H5.24578C3.99561 14 2.93762 13.0765 2.76871 11.8378L1.56355 3H1C0.723858 3 0.5 2.77614 0.5 2.5C0.5 2.22386 0.723858 2 1 2H5ZM6 5.5C6 5.22386 5.77614 5 5.5 5C5.22386 5 5 5.22386 5 5.5V10.5C5 10.7761 5.22386 11 5.5 11C5.77614 11 6 10.7761 6 10.5V5.5ZM8.5 5C8.77614 5 9 5.22386 9 5.5V10.5C9 10.7761 8.77614 11 8.5 11C8.22386 11 8 10.7761 8 10.5V5.5C8 5.22386 8.22386 5 8.5 5ZM3.75954 11.7027C3.86089 12.4459 4.49568 13 5.24578 13H8.75422C9.50432 13 10.1391 12.4459 10.2405 11.7027L11.4272 3H2.57281L3.75954 11.7027Z"/></svg>'
-        /*deleteImage.width = "14";
-        deleteImage.height = "14";
-        deleteImage.viewBox = "0 0 14 14";
-        deleteImage.fill="none";
-        deleteImage.xmlns="http://www.w3.org/2000/svg";
-        deleteImage.innerHTML = '<path d="M6 2H8C8 1.44772 7.55228 1 7 1C6.44772 1 6 1.44772 6 2ZM5 2C5 0.89543 5.89543 0 7 0C8.10457 0 9 0.89543 9 2H13C13.2761 2 13.5 2.22386 13.5 2.5C13.5 2.77614 13.2761 3 13 3H12.4364L11.2313 11.8378C11.0624 13.0765 10.0044 14 8.75422 14H5.24578C3.99561 14 2.93762 13.0765 2.76871 11.8378L1.56355 3H1C0.723858 3 0.5 2.77614 0.5 2.5C0.5 2.22386 0.723858 2 1 2H5ZM6 5.5C6 5.22386 5.77614 5 5.5 5C5.22386 5 5 5.22386 5 5.5V10.5C5 10.7761 5.22386 11 5.5 11C5.77614 11 6 10.7761 6 10.5V5.5ZM8.5 5C8.77614 5 9 5.22386 9 5.5V10.5C9 10.7761 8.77614 11 8.5 11C8.22386 11 8 10.7761 8 10.5V5.5C8 5.22386 8.22386 5 8.5 5ZM3.75954 11.7027C3.86089 12.4459 4.49568 13 5.24578 13H8.75422C9.50432 13 10.1391 12.4459 10.2405 11.7027L11.4272 3H2.57281L3.75954 11.7027Z" fill="#838383">' */
         deleteImage.classList.add("deleteImage");
         const deleteButton = Button({text: "", onClick: onDeleteTask});
         deleteButton.append(deleteImage);
@@ -86,7 +78,6 @@
 
         const checkbox = document.createElement("img");
         checkbox.setAttribute("src", "static/checkboxTask.svg");
-        /*checkbox.onchange = function(){onCompleteTask}; */
         checkbox.addEventListener("click", onCompleteTask);
         let span = document.createElement("span");
         span.innerHTML = `${item}`;
@@ -134,7 +125,7 @@
         return button;
     }
 
-    function SearchInput(searchString, placeholder, changeSearchString) {
+    function SearchInput({searchString, placeholder, changeSearchString}) {
         const searchInput = document.createElement("INPUT");
         searchInput.setAttribute("type", "text");
         searchInput.placeholder = placeholder;
@@ -143,12 +134,58 @@
         return searchInput;
     }
 
-    function TextInput({placeholder, onInput}) {
+    function TextInput({placeholder, onInput }) {
         const textInput = document.createElement("INPUT");
         textInput.setAttribute("type", "text");
         textInput.placeholder = placeholder;
         textInput.oninput = onInput;
         return textInput;
+    }
+
+    function AddNewItemPopup({addItem, closePopup}) {
+        
+        function changeButtonColor() {
+            if (textInput.value !== '') {
+                addTaskButton.disabled = false;
+            };
+            if (textInput.value === '') {
+                addTaskButton.disabled = true;
+            }
+        }
+        function handleAddNewItem() {
+            const value = textInput.value;
+            textInput.value = '';
+            addItem(value);
+        }
+        function handleClosePopup() {
+            textInput.value = '';
+            addTaskButton.disabled = true;
+            closePopup();
+        }
+        const divPopup = document.createElement("div");
+        divPopup.classList.add("divPopup");
+        const popupHeader = document.createElement("h3");
+        popupHeader.innerHTML = "Add New Task";
+        const divPopupButtons = document.createElement("div");
+        divPopupButtons.classList.add("divPopupButtons");
+
+        const textInput = TextInput({placeholder: "New Task", onInput: changeButtonColor});
+        textInput.classList.add("textInput");
+
+        const addTaskButton = Button({text: "Add", onClick: handleAddNewItem});
+        addTaskButton.classList.add("popupSubmitButton");
+        addTaskButton.disabled = true;
+        
+        const closePopupButton = Button({text: "Cancel", onClick: handleClosePopup});
+        closePopupButton.classList.add("popupCancelButton");
+        divPopupButtons.append(closePopupButton, addTaskButton);
+        divPopup.append(popupHeader, textInput, divPopupButtons);
+
+        const focus = () => {
+            textInput.focus();
+        }
+
+        return [divPopup, focus];
     }
     /**
      * App container
@@ -164,16 +201,13 @@
 
         function addNewTask() {
             popupOverlay.style.display = "block";
-            textInput.focus();
-
+            popupInputFocus();
         }
 
-        function addItem() {
-            text = textInput.value;
+        function addItem(text) {
             items.push(text);
             localStorage.setItem('items', JSON.stringify(items));
             setItems(items);
-            textInput.value = '';
             closePopup();
         }
 
@@ -181,46 +215,21 @@
             popupOverlay.style.display = "none";
         }
 
-        function changeButtonColor() {
-            if (textInput.value !== '') {
-                addTaskButton.disabled = false;
-            };
-            if (textInput.value === '') {
-                addTaskButton.disabled = true;
-            }
-        }
-
         const div = document.createElement("div");
         div.classList.add("divHeader");
         const newTaskButton = Button({text: "+ New Task", onClick: addNewTask});
         newTaskButton.classList.add("buttonNewTask");
-        const searchInput = SearchInput(searchString, "Search Task", changeSearchString);
+        const searchInput = SearchInput({searchString: searchString, placeholder: "Search Task", changeSearchString: changeSearchString});
         searchInput.classList.add("searchInput");
         const header = document.createElement("h1");
         header.innerHTML = "To Do List";
+        const [popup, popupInputFocus] = AddNewItemPopup({addItem: addItem, 
+                                                        closePopup: closePopup});
 
         const popupOverlay = document.createElement("div");
         popupOverlay.classList.add("popupOverlay");
-        const divPopup = document.createElement("div");
-        divPopup.classList.add("divPopup");
-        const popupHeader = document.createElement("h3");
-        popupHeader.innerHTML = "Add New Task";
-        const divPopupButtons = document.createElement("div");
-        divPopupButtons.classList.add("divPopupButtons");
-
-        const textInput = TextInput({placeholder: "New Task", onInput: changeButtonColor});
-        textInput.classList.add("textInput");
-
-        const addTaskButton = Button({text: "Add", onClick: addItem});
-        addTaskButton.classList.add("popupSubmitButton");
-        addTaskButton.disabled = true;
-        const closePopupButton = Button({text: "Cancel", onClick: closePopup});
-        closePopupButton.classList.add("popupCancelButton");
-        popupOverlay.append(divPopup);
-        divPopupButtons.append(closePopupButton, addTaskButton);
-        divPopup.append(popupHeader, textInput, divPopupButtons);
-        popupOverlay.style.display = "none";
-
+        popupOverlay.append(popup);
+        popupOverlay.style.display = "none"; 
 
         div.append(header, searchInput, newTaskButton, popupOverlay);
 
@@ -233,21 +242,6 @@
         const [searchString, setSearchString] = useSearchString('');
         localStorage.setItem('items', JSON.stringify(items));
         localStorage.setItem('doneItems', JSON.stringify(doneItems));
-
-        /* function addNewTask() {
-            popupOverlay.style.display = "block";
-
-        }
-
-        function addItem() {
-            text = popupTextInput.value;
-            setItems([...items, `${text}`]);
-        }
-
-        function closePopup() {
-            popupOverlay.style.display = "none";
-        } */
-
         function deleteTask(i) {
             items.splice(i, 1);
             localStorage.setItem('items', JSON.stringify(items));
@@ -275,39 +269,12 @@
         const list = List({items, onDeleteTask: deleteTask, onCompleteTask: completeTask, searchString});
         const doneList = DoneList({doneItems, searchString});
 
-        /* const newTaskButton = Button({text: "+ New Task", onClick: addNewTask});
-        newTaskButton.classList.add("buttonNewTask");
-        const textInput = TextInput(searchString, "Search Task", changeSearchString);
-        textInput.classList.add("textInput"); */
-        /* const header = document.createElement("h1");
-        header.innerHTML = "To Do List"; */
-
         const taskHeader = document.createElement("h3");
         taskHeader.innerHTML = "All Tasks";
         const doneHeader = document.createElement("h3");
         doneHeader.innerHTML = "Completed Tasks";
-        listsDiv.append(taskHeader, list, doneHeader, doneList,)
+        listsDiv.append(taskHeader, list, doneHeader, doneList,);
         
-        /* const popupOverlay = document.createElement("div");
-        popupOverlay.classList.add("popupOverlay");
-        const divPopup = document.createElement("div");
-        divPopup.classList.add("divPopup");
-        const popupHeader = document.createElement("h3");
-        popupHeader.innerHTML = "Add New Task";
-        const divPopupButtons = document.createElement("div");
-        divPopupButtons.classList.add("divPopupButtons");
-        const popupTextInput = TextInput("New Task");
-        popupTextInput.classList.add("popupTextInput");
-        const addTaskButton = Button({text: "Add", onClick: addItem});
-        addTaskButton.classList.add("popupSubmitButton");
-        const closePopupButton = Button({text: "Cancel", onClick: closePopup});
-        closePopupButton.classList.add("popupCancelButton");
-        popupOverlay.append(divPopup);
-        divPopupButtons.append(closePopupButton, addTaskButton);
-        divPopup.append(popupHeader, popupTextInput, divPopupButtons);
-        popupOverlay.style.display = "none"; */
-
-        /* div.append(header,  popupOverlay, textInput, newTaskButton, listsDiv,); */
         div.append(listsDiv,);
         return div;
     }
