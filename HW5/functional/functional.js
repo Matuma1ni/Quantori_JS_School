d16107fd8c6524ac9eaca9d947909e14a97b2c69
Matuma1ni <nadejdav90@gmail.com>
2023-04-18 12:59:44 +0300

    add deleting, updating and completing tasks with json-server

--- HW5/functional/functional.js
+++ HW5/functional/functional.js
@@ -3,6 +3,35 @@
     let doneState = undefined;
     let searchString = undefined;
 
+    const apiClient = {
+        getTodos: async function() {
+            const response = await fetch('http://localhost:3004/tasks');
+            return response.json();
+        },
+        addTodo: async function(title) {
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ title, isCompleted: false })
+            };
+            const response = await fetch('http://localhost:3004/tasks', requestOptions);
+            return response.json();
+        },
+        updateTodo: async function(id, item) {
+            const requestOptions = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(item)
+            };
+            const response = await fetch(`http://localhost:3004/tasks/${id}`, requestOptions);
+        },
+        deleteTodo: async function(id) {
+            const requestOptions = {
+                method: 'DELETE'
+            };
+            const response = await fetch(`http://localhost:3004/tasks/${id}`, requestOptions);
+        }
+    };
     /**
      * Global application state
      * @template T
@@ -18,17 +47,6 @@
         }
 
         return [state, setValue];
-    }
-
-    function useDoneState(initialValue) {
-        doneState = doneState || initialValue;
-
-        function setValue(newValue) {
-            doneState = newValue;
-            renderApp();
-        }
-
-        return [doneState, setValue];
     }
 
     function useSearchString(initialValue) {
@@ -191,13 +209,8 @@
      * @returns {HTMLDivElement} - The app container
      */
     async function AppHeader() {
-        async function getTodos() {
-            const response = await fetch('http://localhost:3004/tasks');
-            return response.json();
-        }
 
-        const [items, setItems] = useState(await getTodos());
-        const todoItems = items.filter(item => item.isCompleted === false);
+        const [_, setItems] = useState(await apiClient.getTodos());
         const [searchString, setSearchString] = useSearchString('');
 
         function changeSearchString() {
@@ -209,9 +222,10 @@
             popupInputFocus();
         }
 
-        function addItem(text) {
-            items.push(text);
-            setItems(items);
+        async function addItem(text) {
+            await apiClient.addTodo(text);
+            const newItems = await apiClient.getTodos();
+            setItems(newItems);
             closePopup();
         }
 
@@ -241,31 +255,23 @@
     }
 
     async function App() {
-        async function getTodos() {
-            const response = await fetch('http://localhost:3004/tasks');
-            return response.json();
-        }
-
-        const [items, setItems] = useState(await getTodos());
-        console.log(items);
+        const [items, setItems] = useState(await apiClient.getTodos());
         const todoItems = items.filter(item => item.isCompleted === false);
-        console.log(todoItems);
         const completedItems = items.filter(item => item.isCompleted === true);
         const [searchString, setSearchString] = useSearchString('');
 
-        async function deleteTask(i) {
-            items.splice(i, 1);
-            setItems(items);
+        async function deleteTask(id) {
+            await apiClient.deleteTodo(id);
+            const newItems = await apiClient.getTodos();
+            setItems(newItems);
         }
 
-        function completeTask(i) {
-            let element = items.slice(i, (i+1));
-            items.splice(i, 1);
-            setItems(items);
-        }
-
-        function changeSearchString() {
-            setSearchString(textInput.value);
+        async function completeTask(id) {
+            const item = items.filter(item => item.id === id)[0];
+            item.isCompleted = true;
+            await apiClient.updateTodo(id, item);
+            const newItems = await apiClient.getTodos();
+            setItems(newItems);
         }
 
         const div = document.createElement("div");
