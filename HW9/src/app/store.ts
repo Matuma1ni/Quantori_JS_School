8db3e7eebf3b745ba9249d8bc7e92b54e81487e5
Matuma1ni <nadejdav90@gmail.com>
2023-05-10 13:50:29 +0300

    Add tabs sync

--- HW9/src/app/store.ts
+++ HW9/src/app/store.ts
@@ -1,11 +1,17 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import toDosReducer from "../features/todo/todoSlice";
+import {createStateSyncMiddleware, initMessageListener} from "redux-state-sync";
+
+const reduxStateSyncConfig = {};
 
 export const store = configureStore({
   reducer: {
     toDos: toDosReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(createStateSyncMiddleware(reduxStateSyncConfig)),
 });
+
+initMessageListener(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
